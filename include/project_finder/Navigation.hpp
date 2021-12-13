ee98f1af96de13ee0850dfb0facf1c53459f8d4b
Naitri <naitrir21@gmail.com>
2021-12-13 12:46:00 -0500

    3.9.2 Modified Navigation header file

--- include/project_finder/Navigation.hpp
+++ include/project_finder/Navigation.hpp
@@ -38,7 +38,21 @@
 
 #include <actionlib/client/simple_action_client.h>
 #include <move_base_msgs/MoveBaseAction.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+
+#include <opencv4/opencv2/opencv.hpp>
+
 #include <project_finder/Pose.hpp>
+#include <project_finder/FinderBot.hpp>
+#include <project_finder/Detector.hpp>
+
+
+
+
+
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction>
+ MoveBaseClient;
 
 /**
  * @brief Navigation class
@@ -51,7 +65,7 @@ class Navigation {
   * @brief Construct a new Navigation object
   * 
   */
-    Navigation();
+    explicit Navigation(ros::NodeHandle nh, std::vector<FinderBot>& robots);
 
     /**
      * @brief Destroy the Navigation object
@@ -66,13 +80,68 @@ class Navigation {
     * @return true 
     * @return false 
     */
-    bool move(Pose& dest_location);
+    void move_bots();
+    static move_base_msgs::MoveBaseGoal get_destination_goal(
+      std::shared_ptr<FinderBot>& robot);
 
  private:
+  void initialize_client(std::shared_ptr<FinderBot>& robot,
+  std::unique_ptr<MoveBaseClient>& robot_client);
+   bool detect_human();
+   void update_robot(std::unique_ptr<MoveBaseClient>& robot_client,
+    std::shared_ptr<FinderBot>& robot);
+   void send_goal(std::unique_ptr<MoveBaseClient>& robot_client,
+    std::shared_ptr<FinderBot>& robot,  
+    move_base_msgs::MoveBaseGoal& robot_goal);
+   bool mission_impossible_complete();
 
-typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
-    std::unique_ptr<MoveBaseClient> action_server_;
-     move_base_msgs::MoveBaseGoal goal_;
+ private:
+  std::shared_ptr<FinderBot> robot1_;
+  std::shared_ptr<FinderBot> robot2_;
+  std::shared_ptr<FinderBot> robot3_;
+  std::shared_ptr<FinderBot> robot4_;
+  std::shared_ptr<FinderBot> robot5_;
+  std::shared_ptr<FinderBot> robot6_;
+  std::shared_ptr<FinderBot> robot7_;
+  std::shared_ptr<FinderBot> robot8_;
+  std::shared_ptr<FinderBot> robot9_;
+  std::shared_ptr<FinderBot> robot10_;
+  std::shared_ptr<FinderBot> robot11_;
+  std::shared_ptr<FinderBot> robot12_;
+  std::shared_ptr<FinderBot> robot13_;
+  std::shared_ptr<FinderBot> robot14_;
+  std::shared_ptr<FinderBot> robot15_;
+  std::shared_ptr<FinderBot> robot16_;
+  std::shared_ptr<FinderBot> robot17_;
+  std::shared_ptr<FinderBot> robot18_;
+  std::shared_ptr<FinderBot> robot19_;
+  std::shared_ptr<FinderBot> robot20_;
+
+  std::unique_ptr<MoveBaseClient> robot1_client_;
+  std::unique_ptr<MoveBaseClient> robot2_client_;
+  std::unique_ptr<MoveBaseClient> robot3_client_;
+  std::unique_ptr<MoveBaseClient> robot4_client_;
+  std::unique_ptr<MoveBaseClient> robot5_client_;
+  std::unique_ptr<MoveBaseClient> robot6_client_;
+  std::unique_ptr<MoveBaseClient> robot7_client_;
+  std::unique_ptr<MoveBaseClient> robot8_client_;
+  std::unique_ptr<MoveBaseClient> robot9_client_;
+  std::unique_ptr<MoveBaseClient> robot10_client_;
+  std::unique_ptr<MoveBaseClient> robot11_client_;
+  std::unique_ptr<MoveBaseClient> robot12_client_;
+  std::unique_ptr<MoveBaseClient> robot13_client_;
+  std::unique_ptr<MoveBaseClient> robot14_client_;
+  std::unique_ptr<MoveBaseClient> robot15_client_;
+  std::unique_ptr<MoveBaseClient> robot16_client_;
+  std::unique_ptr<MoveBaseClient> robot17_client_;
+  std::unique_ptr<MoveBaseClient> robot18_client_;
+  std::unique_ptr<MoveBaseClient> robot19_client_;
+  std::unique_ptr<MoveBaseClient> robot20_client_;
+
+  std::unique_ptr<acme::Detector> detector_;
+  
+  ros::NodeHandle nh_;
+  cv::Mat opencv_img_;
 };
 
 #endif  // INCLUDE_NAVIGATION_HPP_
