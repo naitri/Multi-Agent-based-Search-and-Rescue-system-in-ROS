4badea4886c43e6a859254bbc432b14eb7da21a6
Naitri <naitrir21@gmail.com>
2021-12-13 13:04:04 -0500

    3.9.1 Updated FinderBot header file with comments

--- include/project_finder/FinderBot.hpp
+++ include/project_finder/FinderBot.hpp
@@ -24,7 +24,8 @@
 
 /**
  * @file FinderBot.hpp
- * @author Phase 2 - Mayank Joshi (driver) and Naitri Rajyaguru (navigator)
+ * @author Mayank Joshi
+ * @author Naitri Rajyaguru
  * @brief class declaration for FinderBot
  * @version 0.1
  * 
@@ -62,30 +63,135 @@ class FinderBot {
      */
     ~FinderBot();
    
+   /**
+    * @brief Get the namespace object
+    * 
+    * @return std::string 
+    */
    std::string get_namespace();
+
+   /**
+    * @brief Set the waypoint object
+    * 
+    * @param waypoint 
+    */
    void set_waypoint(Pose waypoint);
+
+   /**
+    * @brief Set the fire exit object
+    * 
+    * @param fire_exit 
+    */
    void set_fire_exit(Pose fire_exit);
+
+   /**
+    * @brief Get the waypoint object
+    * 
+    * @return Pose 
+    */
    Pose get_waypoint();
+
+   /**
+    * @brief Get the fire exit object
+    * 
+    * @return Pose 
+    */
    Pose get_fire_exit();
+
+   /**
+    * @brief udpate destination of the robot
+    * 
+    * @param dest 
+    */
    void update_destination(Pose dest);
+
+   /**
+    * @brief Get the destination object
+    * 
+    * @return Pose 
+    */
    Pose get_destination();
+
+   /**
+    * @brief Set the send goal state object
+    * 
+    * @param send_goal 
+    */
    void set_send_goal_state(bool send_goal);
+
+   /**
+    * @brief Get the send goal state object
+    * 
+    * @return true 
+    * @return false 
+    */
    bool get_send_goal_state();
+
+   /**
+    * @brief Set the mission status object
+    * 
+    * @param status 
+    */
    void set_mission_status(bool status);
+
+   /**
+    * @brief Get the mission status object
+    * 
+    * @return true 
+    * @return false 
+    */
    bool get_mission_status();
+
+   /**
+    * @brief Set the home location object
+    * 
+    * @param home_sweet_home 
+    */
    void set_home_location(Pose home_sweet_home);
+
+   /**
+    * @brief Get the home location object
+    * 
+    * @return Pose 
+    */
    Pose get_home_location();
+
+   /**
+    * @brief increase robot mission completed count
+    * 
+    */
    void increment_mission_count();
+
+   /**
+    * @brief Get the mission count object
+    * 
+    * @return int 
+    */
    int get_mission_count();
 
  private:
+ // variable to store the robot namespace
    std::string namespace_;
+  
+   // variable to store the robot waypoint
    Pose waypoint_;
+
+    // variable to store the robot fire exit location
    Pose fire_exit_;
+
+    // variable to store the robot destination
    Pose destination_;
+
+    // variable to store the robot send goal status
    bool send_goal_state_;
+
+    // variable to store the robot mission status
    bool mission_accomplished_;
+
+    // variable to store the robot home location
    Pose home_location_;
+
+    // variable to store the robot mission count
    int mission_count_;
    
 };
